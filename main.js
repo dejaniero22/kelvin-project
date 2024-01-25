3491917ef7750f7a95499e1dbeeb7a7498c99da6
dejaniero22 <122594571+dejaniero22@users.noreply.github.com>
2024-01-24 22:01:44 -0800

    fix: remove redundant lines of code
    Remove redundant Math.floor calls by wrapping formulas with constants in the same line.

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 // Kelvin is a constant representing today's forecasted temperature.
-const kelvin = 0;
+const kelvin = 283;
 
 // Celsius is calculated as 273 less than Kelvin.
 const celsius = kelvin - 273;
 
 // Fahrenheit is derived from Celsius using a formula.
-let fahrenheit = celsius * (9/5) + 32;
-
-//This takes the var fahrenheit and applys the floor method to it and then stores the value in the var fahrenheit.
-fahrenheit = (Math.floor(fahrenheit));
+const fahrenheit = Math.floor(celsius * (9/5) + 32);
 
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
 // Newton scale, for the curious minds.
 
-let newton = celsius * (33/100);
-
-newton = (Math.floor(newton));
+const newton = Math.floor(celsius * (33/100));
 
 console.log(`The temperature is ${newton} degrees Newton.`);
